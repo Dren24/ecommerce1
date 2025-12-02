d658b2fe055bfd7de3fafc29f0474ed69a949105
Dren24 <baricuatroaldren98@gmail.com>
2025-12-02 21:56:14 +0800

    chp14

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,52 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use App\Models\Product;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Product Details')]
 class ProductDetailPage extends Component
 {
+    public $slug;
+    public $quantity = 1;
+
+    public function mount($slug)
+    {
+        $this->$slug = $slug;
+    }
+
+    public function increaseQty()
+    {
+        $this->quantity++;
+    }
+
+    public function decreaseQty()
+    {
+        if ($this->quantity > 1) {
+            $this->quantity--;
+        }
+    }
+    public function addToCart($product_id)
+    {
+        $total_count = CartManagement::addItemToCartWithQty($product_id, $this->quantity);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        LivewireAlert::title('Product Added Succesfully!')
+            ->position('bottom-end')
+            ->timer(3000)
+            ->toast()
+            ->show();
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
